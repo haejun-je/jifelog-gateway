b3c383c7012c54533d3e97609f3a16ed0a32c626
haejun-je <jhjun07@gmail.com>
2026-05-22 19:36:22 +0900

    feat: jwt expiresAt 제거 및 세션 매핑 수정

--- src/main/kotlin/com/jifelog/gateway/filter/SessionToJwtGlobalFilter.kt
+++ src/main/kotlin/com/jifelog/gateway/filter/SessionToJwtGlobalFilter.kt
@@ -39,6 +39,7 @@ class SessionToJwtGlobalFilter(
         return sessionRepository.findById(sessionId)
             .flatMap { session ->
                 val internalJwt = jwtGenerator.generate(session)
+                println("internalJwt = ${internalJwt}")
 
                 val requestWithJwt = sanitizedExchange.request.mutate()
                     .headers { it.set(jwtGenerator.headerName(), internalJwt) }
